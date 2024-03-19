98b2d462f27f19802625ff75a88067fd7f4cd935
cestmoi <lucas.minaud@outlook.fr>
2024-03-19 18:55:54 +0100

    erreur image bouton

--- morpion.py
+++ morpion.py
@@ -1,6 +1,7 @@
 import tkinter
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, Canvas
+from PIL import ImageTk, Image
 
 class Morpion:
     def __init__(self, main_win):
@@ -11,14 +12,34 @@ class Morpion:
         self.win = False
         self.boutons = []
 
+
         self.morpion()
 
     def winner(self):
         if self.win is False:
             self.win = True
+            self.main_win.label.destroy()
             self.main_win.label = tk.Label(self.main_win, text='le Gagnant est ' + self.current_player)
             print(self.current_player, "win")
             self.delete_button_worker(self.boutons)
+            self.main_win.label.grid(column=0)
+
+
+            image = Image.open("./assets/raton.jpg")
+            image = image.resize((400,300), Image.NEAREST)
+
+            self.bouton = ttk.Button(
+                self.main_win,
+                text="teuteu",
+                command=self.main_win.main,
+                image=ImageTk.PhotoImage(image)
+            )
+            canvas = Canvas(self.bouton, width=400, height=300)
+            canvas.image = ImageTk.PhotoImage(image)
+            canvas.create_image(0, 0, image=canvas.image, anchor='nw')
+
+            self.bouton.grid()
+
 
 
     def swi_player(self):
