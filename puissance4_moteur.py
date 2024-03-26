05a0feaf99681d7777f32cc1e10216d3dded2eae
cestmoi <lucas.minaud@outlook.fr>
2024-03-26 23:38:09 +0100

    verif horiz

--- puissance4_moteur.py
+++ puissance4_moteur.py
@@ -5,6 +5,23 @@ class Player(Enum):
     TACOS = -1
     AUCUN = 0
 
+class Coords:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other_coords):
+        return Coords(self.x + other_coords.x,
+                      self.y + other_coords.y)
+
+    def inside(self, limits_x, limits_y):
+        return (limits_x[0] >= self.x <= limits_x[1]
+            and limits_y[0] >= self.y <= limits_y[1])
+
+    def get_cell(self, board):
+        print(self.x , self.y)
+        return board[self.y][self.x]
+
 class P4_moteur:
     def __init__(self):
         self.current_player = Player.RATON
@@ -37,17 +54,34 @@ class P4_moteur:
     def verif_win(self, player: Player, click_col : int, current_row : int) -> bool:
         """verifie si le pion poser créé une ligne gagnante"""
         #verification de victoire VERTICAL
+        print("click sa mere")
         count = 1
-        if current_row >2:
-            return False
         for k in range(1,4):
-            if self.board[current_row + k][click_col] == player:
+            if current_row+k <=5 and self.board[current_row + k][click_col] == player:
                 count += 1
-            else:
-                return False
-        if not count > 2:
-            return False
-        self.winner = self.current_player
-        return True
+        if count > 3:
+            self.winner = self.current_player
+            return True
+        #verif horizontal par recurtion
+        countH = 0
+        directionD = Coords(1, 0)
+        directionG = Coords(-1, 0)
+        position = Coords(current_row, click_col)
+        countH = self.recursion(position, directionD, player)
+        print(countH)
+        countH += self.recursion(position, directionG, player)
+        print(countH)
+        if countH > 4:
+            self.winner = self.current_player
+            return True
+        return False
+
+    def recursion(self, position: Coords, direction: Coords, player: Player) -> int:
+        new_pos = position + direction
+        print(position.x)
+        if new_pos.inside([-1, 6], [-1, 7]) or position.get_cell(self.board) != player:
+            return 0
+        return 1 + self.recursion(new_pos, direction, player)
+
     def reset_board(self):
         self.board = [[Player.AUCUN for i in range(7)] for j in range(6)]
